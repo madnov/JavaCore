4611d947ca567ca603ed0a196f430f8030a91b3b
Max <madnovos@mail.ru>
2024-01-17 15:19:58 +0300

    first commit

---
+++ HomeWorik_2/Task_1.java
@@ -0,0 +1,21 @@
+//1. Написать метод, возвращающий количество чётных элементов массива.
+//
+//countEvens([2, 1, 2, 3, 4]) → 3
+//countEvens([2, 2, 0]) → 3
+//countEvens([1, 3, 5]) → 0
+
+public class Task_1 {
+    public static void main(String[] args) {
+        System.out.println(countEvents);
+    }
+
+    public int countEvents(int[] nums) {
+        int count = 0;
+        for (int i = 0; i < ; i++) {
+            if (nums[i] % 2 == 0) {
+                count++;
+            }
+        }
+        return count
+    }
+}
